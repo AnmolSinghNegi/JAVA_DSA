3851a73eae45351e880b3642f4aab525bece0da5
AnmolSinghNegi <99644009+AnmolSinghNegi@users.noreply.github.com>
2022-08-10 12:10:03 +0530

    Add files via upload

---
+++ Array/Majority Element.java
@@ -0,0 +1,43 @@
+package Array2;
+
+import java.util.Scanner;
+
+public class majorityElement {
+	public static int[]input(){
+		System.out.print("Enter the size of array : ");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int []array = new int[size];
+		for(int i=0;i<size;i++) {
+			System.out.print("Enter the "+(i+1)+" element of array : ");
+			array[i]= sc.nextInt();
+		}
+		sc.close();
+		return array;
+	}
+	public static int majorityElement(int[] nums) {
+		int count=0;
+        int ans=nums[0];
+        for(int ele:nums){
+        	 if(count==0){
+                   ans=ele;
+                   count++;
+               }
+            if(ans==ele){
+                count++;
+                continue;
+            }
+            else{
+                --count;
+            }
+        }
+        return ans;
+	}
+	public static void main(String[] args) {
+		int []array =input();
+		int majority=majorityElement(array);
+		System.out.print("The majority element is : "+majority);
+		
+	}
+
+}
